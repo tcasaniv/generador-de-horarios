90529ec29df6595854fb0e60608db39a298683bb
tcasaniv <99104321+tcasaniv@users.noreply.github.com>
2025-08-06 17:21:13 -0500

    Change tailwindcss from cdn to local for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
-export default {
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  plugins: [tailwindcss()],
   base: "/generador-de-horarios/",
-};
+});
